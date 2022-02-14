04a7bb08183a091351bc9dccfd1f3fbfdfc9d5c3
Edward Naidoo <securedbread@protonmail.com>
2022-02-13 18:59:39 -0800

    Made most of the Navbar
    I have given all of the necessary components (not to be confused with react components) to the navigation, where only small changes will be needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ReactComponent as NasaLogo} from './NasaLogo-96.svg'
+
+function Navbar() {
+  return (
+    <>
+      <header>
+        <div className="navbar__background">
+          <div className="logo__title">
+            <NasaLogo />
+            <h3>Astronomy Picture of the Day</h3>
+          </div>
+          <div className="nav__links">
+            <ul><li><a href="#">Home</a></li></ul>
+            <ul><li><a href="#">News</a></li></ul>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar />
   );
 }
 
