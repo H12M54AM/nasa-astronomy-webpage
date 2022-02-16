ca66eb1ffb49c3ddf3749b330a24e1b32a7815a8
Edward Naidoo <securedbread@protonmail.com>
2022-02-16 08:04:13 -0800

    Updated Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function Navbar() {
       <header>
         <div className="navbar__background">
           <div className="logo__title">
-            <NasaLogo />
+            <NasaLogo id='nasa__logo'/>
             <h3>Astronomy Picture of the Day</h3>
           </div>
           <div className="nav__links">
